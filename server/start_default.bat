c3f34938dd44a6e0ccd8a5233c1388c8df096a2d
CarpeDiem1984 <kirya.panovitsa@gmail.com>
2021-04-06 23:54:10 +0300

    CarpeDiem

---
+++ server/start_default.bat
@@ -0,0 +1,3 @@
+@echo off
+C:/Users/MadKek/Desktop/unity/server//run.cmd +set serverProfile "default"
+pause
---
+++ start.bat
@@ -0,0 +1,2 @@
+cd C:\Users\MadKek\Desktop\unity\server\server_data
+C:\Users\MadKek\Desktop\unity\server\run.cmd +exec server.cfg
